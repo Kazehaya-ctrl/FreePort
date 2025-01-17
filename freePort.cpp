8a9c4a78950b6a44217966e72b150d3c7b2469c5
Kazehaya-ctrl <mahitomaki96@gmail.com>
2025-01-17 22:56:05 +0530

    upt: Working

--- freePort.cpp
+++ freePort.cpp
@@ -21,13 +21,13 @@ int main(int args, char* argv[]) {
     std::string command = "netstat -ano | findstr :" + portNumber;
     std::string result = commandExecution(command);
     std::cout << result << std::endl;
-    std::string exclusion = "LISTENING";
-    std::string pointerToExclusion = exclusion.c_str();
-    int lengthOfExclusion = pointerToExclusion.length();
-    int exists = result.find(exclusion);
-    std::string PID = result.substr(exists + lengthOfExclusion + 4, 10);
-    std::string command2 = "taskkill /PID " + PID + " /F";
-    std::string secondResult = commandExecution(command2);
+    size_t pidStart = result.find_last_of(" ") + 1;
+    std::string PID = result.substr(pidStart);
+    PID.erase(PID.find_last_not_of(" \t\n\r") + 1);
+    std::string killCommand = "taskkill /PID " + PID + " /F";
+
+    std::string secondResult = commandExecution(killCommand);
+    std::cout << secondResult << std::endl;
   } catch (const std::exception& e) {
     std::cout << "Error: " << e.what() << std::endl;
   }
